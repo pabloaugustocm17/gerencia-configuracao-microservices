74c81e0790ce52fd41886da7d0f72d1558611260
Nikolas Louret <nikoul.ret@gmail.com>
2024-04-07 12:21:59 -0300

    feat: add and style inputs

--- babilonia/src/app/app.routes.ts
+++ babilonia/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'book-register',
+    path: 'book-registration',
     loadComponent: () =>
       import('./features/book-registration/book-registration.component').then(
         (m) => m.BookRegistrationComponent
--- babilonia/src/app/features/book-registration/book-registration.component.ts
+++ babilonia/src/app/features/book-registration/book-registration.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import {
   FormControl,
   FormGroup,
@@ -11,13 +15,39 @@ import {
 @Component({
   selector: 'app-book-registration',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonModule,
+  ],
+  providers: [
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+  ],
   templateUrl: './book-registration.component.html',
   styleUrl: './book-registration.component.css',
 })
 export class BookRegistrationComponent {
   form = new FormGroup({
     name: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    author: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    launch_date: new FormControl<Date>(new Date(), {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    description: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    price: new FormControl<number>(0, {
       nonNullable: true,
       validators: Validators.required,
     }),
